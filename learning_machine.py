f38df71df8fd86a74de363c60e006feaf9306be4
darkcentaure <nielsintveld@PC-Don.lan>
2021-09-29 14:44:20 +0200

    update

--- learning_machine.py
+++ learning_machine.py
@@ -1,94 +1,86 @@
+import numpy as np
+
+
 class LearningMachine:
-    
     def __init__(self, data_class):
-        
-        self.name = None
-        
-        self.input = data_class
-        
-        self.labels = data_class.labels
-        
-        self.methods = ["LDA", "RDA"]
-        
-        self.output = np.full(np.shape(self.labels), None)
-        self.metrics = {}
-        
-        self.fitting_parameters = {}
-        
+        self.name = None  # name of ML algorithm
         self.model = None
-        
-        
+
+        self.data_class = data_class  # training data object
+        self.output = np.full(np.shape(self.data_class.labels), None)  # output of ML algorithm
+
+        self.metrics = {}  # dict of metrics, e.g. accuracy
+        self.fitting_parameters = dict()
+        self.run = 0
+        pass
+
     def __str__(self):
         return str(self.name)
-    
-        
+
     def _fit(self):
-        
-        self.fitting_parameters = {}
-        
+        # self.fitting_parameters = {}
         pass
-    
-        
+
     def _predict(self):
-        
-        self.output = self.output
-        
+        # self.output = self.output
         pass
-        
-        
+
     def _evaluate_training(self):
-        
-        self.metrics = {}
-        
+        # self.metrics = {}
         pass
-    
+
     def _evaluate_testing(self):
-        
-        self.metrics = {}
-        
+        # self.metrics = {}
         pass
-        
+
+
 class LDA(LearningMachine):
-    
     def __init__(self, data):
         super().__init__(data)
+
         self.name = "LDA"
-        
+
         from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-        
         self.model = LinearDiscriminantAnalysis()
 
+        # self._fit()
         pass
 
     def _fit(self):
-        
-        self.fitting_parameters = self.model.fit(self.input.df, self.input.labels).get_params()
-        
+        self.fitting_parameters[self.run] = self.model.fit(self.data_class.df, self.data_class.labels)
+        # print(f"The {self.name} model has been trained on the given data")
+        # print("with parameters:" + str(self.model.get_params()))
+        self.run += 1
+        pass
+
+    def _refit(self):
+        self.fitting_parameters = self.model.fit(self.data_class.df, self.data_class.labels).get_params()
         print(f"The {self.name} model has been trained on the given data")
         pass
-    
+
     def _predict(self, X):
-        
         """
         X is a dataframe containing the features of the samples for which we want to predict their labels
         """
-        
+
         self.model.predict(X)
-        
+        print(f"The {self.name} model has predicted X")
         pass
-    
-    def _evaluate_training(self, measure="accuracy"):
-        
+
+    def _evaluate_training(self, measure="accuracy") -> float:
         if measure == "accuracy":
-            sc = self.model.score(self.input.df,self.labels)
-            self.metrics["Train_Acc"]  = sc
-        
-    def _evaluate_testing(self, X, Y, measure="accuracy"):
-        
-        """
-        X is a dataframe containing the features of the samples for which we want to predict their labels
-        Y is a dataframe containing the true labels for the data X  (1 = like, 0 = dislike)
-        """
-        
-        
-        self.model.score(X, Y)
+            score = self.model.score(self.data_class.df, self.data_class.labels)
+            self.metrics[str(self.run)+"_train_Acc"] = score
+
+        print(f"The {self.name} model has been trained on the given data")
+        return score
+
+    # def _evaluate_testing(self, X, Y, measure="accuracy"):
+    #     """
+    #     X is a dataframe containing the features of the samples for which we want to predict their labels
+    #     Y is a dataframe containing the true labels for the data X  (1 = like, 0 = dislike)
+    #     """
+    #     print(f"The {self.name} model has been trained on the given data")
+    #
+    #     self.model.score(X, Y)
+    # pass
--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello World!")
+from data import Data
+from learning_machine import LearningMachine, LDA
+import numpy as np
+
 
 if __name__ == "__main__":
+    #initializing data
+    data = Data('project_train.csv')
+    data.df.loc[84, "energy"] = 0.734
+    data.df.loc[94, "loudness"] = -6.542
+    data._preprocess()
 
-    main()
+    #creating Learning Machine
+    lda = LDA(data)
+    lda._fit()
+    # for i in ("svd","lsqr","eigen"):
+    #     lda.model.solver = i
+    #     lda._fit()
+    #     score = lda._evaluate_training()
+    #     print(i+"-score:"+str(score))
+    #     print(lda.model.get_params())
+    #     print()
+    # print(lda.metrics)
+
+    prediction = lda.model.predict(lda.data_class.df)
+
+    color_tp = [prediction == 1 and lda.data_class.labels == 1]
+    print(color_tp)
+    exit()
+    print(prediction==lda.data_class.labels)
+
+
